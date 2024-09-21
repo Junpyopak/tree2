395bbc72b3f56bd4a2c73687e197d4ce52c27911
gf21\User <sbsgameas@gmail.com>
2024-09-21 16:07:12 +0900

    행동트리 동기화

---
+++ Assets/Script/CAgent.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor;
+using UnityEngine;
+
+public class CAgent : MonoBehaviour
+{
+    [SerializeField]
+    Vector3 mTargetPosition = Vector3.zero;
+
+    //속력
+    float mSpeed = 1f;
+    
+    //행동트리 최상단 루트노드
+    Sequence mRootNode = null;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //상태를 결정하는 부분이 트리구조의 코드형태로 국지화된다.
+        //즉.'변경의 국지화'가 일어난다.
+
+
+        //행동트리를 구축
+        //level 2
+        ActionNode tANMove = new ActionNode(DoMove);
+        List<Node> tLevel_2 = new List<Node>();
+        tLevel_2 .Add(tANMove);
+
+        //Level 1
+        mRootNode = new Sequence(tLevel_2);
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mRootNode.Evaluate();
+    }
+
+    //실제 수행할 행동
+   NodeStates DoMove()
+    {
+        Debug.Log("DoMove");
+
+        //선형보간을 기반으로 이동하는 함수
+       this.transform.position =  Vector3.MoveTowards(this.transform.position, mTargetPosition, mSpeed * Time.deltaTime);
+
+        if(Vector3.Distance(this.transform.position,mTargetPosition) <=0.01f)
+        {
+            Debug.Log("<color='red'>Move complite!!</color>");
+
+            return NodeStates.SUCCESS;
+        }
+        else
+        {
+            return NodeStates.FAILURE;
+        }
+        
+    }
+
+     void DrawGizmo()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(mTargetPosition, Vector3.one * 0.5f);
+    }
+}
---
+++ Assets/Script/bts_base/Selector.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Selector : Node 
+{
+    /** The child nodes for this selector */
+    protected List<Node> m_nodes = new List<Node>();
+
+
+    /** The constructor requires a lsit of child nodes to be 
+     * passed in*/
+    public Selector(List<Node> nodes) 
+    {
+        m_nodes = nodes;
+    }
+
+    /* If any of the children reports a success, the selector will
+     * immediately report a success upwards. If all children fail,
+     * it will report a failure instead.*/
+    public override NodeStates Evaluate() 
+    {
+        foreach (Node node in m_nodes) 
+        {
+            switch (node.Evaluate()) 
+            {
+                case NodeStates.FAILURE:
+                    continue;
+                case NodeStates.SUCCESS:
+                    m_nodeState = NodeStates.SUCCESS;
+                    return m_nodeState;
+                //자식노트들중에 하나라도 true를 리턴하면,자기자신도 true 를 리턴하며 그 즉시종료
+                case NodeStates.RUNNING:
+                    m_nodeState = NodeStates.RUNNING;
+                    return m_nodeState;
+                default:
+                    continue;
+            }
+        }
+        m_nodeState = NodeStates.FAILURE;
+        return m_nodeState;
+    }
+}
